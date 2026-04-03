8896ec9427ae7554401d5ad388896410090425c1
faizxdwm <faizxdwm@gmail.com>
2026-04-04 00:08:11 +0700

    fix error and lib

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,7 +1,18 @@
 import { Client } from '@notionhq/client'
 
+const notionToken = process.env.NOTION_TOKEN
+const databaseId = process.env.NOTION_DATABASE_ID
+
+if (!notionToken) {
+    throw new Error('Missing NOTION_TOKEN in environment variables')
+}
+
+if (!databaseId) {
+    throw new Error('Missing NOTION_DATABASE_ID in environment variables')
+}
+
 export const notion = new Client({
-    auth: process.env.NOTION_TOKEN,
+    auth: notionToken,
 })
 
-export const databaseId = process.env.NOTION_DATABASE_ID!
+export { databaseId }
